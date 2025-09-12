4da259f136c3c4a8431f352a7ece812868dcca44
Ricardo Jorge <ricardolemos18@hotmail.com>
2025-09-12 04:35:31 -0300

    Deploy bug fix

--- src/utils/config.jsx
+++ src/utils/config.jsx
@@ -1,5 +1,5 @@
 //let localUrl = "http://localhost:8001/api/v1";
-let serverUrl = "https://api.strugglerdev.space/api/v1";
+let serverUrl = "https://api.strugglerdev.space";
 
 export const api = serverUrl;
 
